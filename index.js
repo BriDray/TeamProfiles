468c50947d6fc711e9e2197e0f25ebc11a2ed632
Bri Dray <stanggurl02@gmail.com>
2023-02-27 20:59:22 -0800

    added question arrays, added some html and styling.

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
-// Declare employees as an empty array to be filled with user input
-const employees = [
-      {
+// WHAT SET OF QUESTIONS WILL WE NEED?
+const Position = [
+ {
         type: 'list',
         message: 'What is your Position?',
         name: 'role',
         choices: ['Employee', 'Manager', 'Engineer', 'Intern'],
-      },
+      }
+]
+
+// QUESTIONS FOR THE EMPLOYEES
+const employees = [
       {
         type: 'input',
         name: 'name',
@@ -31,23 +35,82 @@ const employees = [
         name: 'email',
         message: "Enter your E-mail?",
       },
-      {
-        type: 'input',
-        name: 'officeNumber',
-        message: "what's your office number?",
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: "what's your Github Username?",
-      },
-      {
-        type: 'input',
-        name: 'school',
-        message: "What school are you/did you attend?",
-      },
 ];
 
+// QUESTIONS FOR THE INTERNS
+const interns = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your First and Last Name?",
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What's your Employee Id?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Enter your E-mail?",
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: "What school are you attending?",
+  },
+
+]
+
+// QUESTIONS FOR THE MANAGERS
+const managers = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your First and Last Name?",
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What's your Employee Id?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Enter your E-mail?",
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: "What's your Office Number?",
+  },
+]
+
+// QUESTIONS FOR THE ENGINEERS
+const Engineers = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What's your First and Last Name?",
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What's your Employee Id?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Enter your E-mail?",
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: "Whats your Github User name?",
+  },
+]
+
+// WRITING TO THE FILE
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, function (err) {
       if (err) throw err;
@@ -55,13 +118,46 @@ function writeToFile(fileName, data) {
     });
   }
 
+// CHOOSING WHAT FILE WE WANT OUR ANSWERS TO WRITE TO. 
 function init() {
     console.log("initialize")
     inquirer.prompt(questions).then((answers) => {
       writeToFile("generateHTML.js",
       `
-      
+// HEADER
+<header style="height: 125px; width: 100%; background-color: darkcyan; margin-top: 0%;">
+<h1 Style="font-size: 100px; text-align: center; font-weight: bold; color:azure">Meet The Team</h1>
+</header>
 
+// SECOND PAGE TITLE
+<h2 Style="font-size: 65px; text-align: center; font-weight: bold; color:black; margin-top: 10px;">Team Profiles</h2>
+
+// WHERE I WANT THE CARDS TO GENERATE
+<div class="cardContainer">
+
+// INLINE STYLE FOR THE CARDS AND CARDCONTAINER
+<style>
+        .cardContainer {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-evenly; 
+            margin: 15px 0 15px 0;
+        }
+        .profileCard {
+            display: inline-block;
+            width:350px;
+            height:375px;
+            border-top: 10px;
+            border-color: solid darkmagenta;
+            background-color: antiquewhite;
+            text-align: center;
+
+        }
+</style>
+</div>
+
+// FOOTER
+<footer style="height: 45px; width: 100%; background-color: darkcyan;"></footer>
 
       `)
     });
@@ -70,3 +166,4 @@ function init() {
   
   // Function call to initialize app
   init();
+ var Anthony = new Manager ("anthony")
